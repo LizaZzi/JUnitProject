e6a286dfe80f400aef77847626e6d7f61496cf5a
LizaZzi <esvirshko@yandex.ru>
2022-08-07 19:22:52 +0300

    load project

---
+++ app/src/test/java/JUnitProject/SomeClassTest.java
@@ -0,0 +1,14 @@
+package JUnitProject;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SomeClassTest {
+    @Test
+    public void testSum() {
+        var expected = 5;
+        var actual = SomeClass.sum(3, 2);
+        assertEquals(expected, actual);
+    }
+}
